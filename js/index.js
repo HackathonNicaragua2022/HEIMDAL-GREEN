660798d763bce09a25de2d1d6da4c163bbeea0b1
Joorkar Romero <joorkarobrajham@gmail.com>
2022-10-23 19:19:09 -0600

    home in progress

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@ const checkBurger = document.getElementById('check')
 const background = document.getElementById('background')
 const burger = document.getElementById('burger')
 const logoClose = document.getElementById('logoClose')
+const navMenu = document.getElementById('nav-menu')
 
 checkBurger.addEventListener('click', function () {
   if (checkBurger.checked === false) {
     background.style.backgroundColor = '#4EA539';
     burger.src = '../assets/burger-menu.svg'
     logoClose.src = '../assets/logo-mobile.svg'
+    navMenu.style.display = 'none'
   } else {
     background.style.backgroundColor = 'transparent';
     burger.src = '../assets/burger-menu-close.svg'
     logoClose.src = '../assets/logo-mobile-close.svg'
+    navMenu.style.display = 'flex'
   }
 })
 
