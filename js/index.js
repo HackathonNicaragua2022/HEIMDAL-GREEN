cf8964c60e203338681a9059eda0bea4198ee10a
Joorkar Romero <joorkarobrajham@gmail.com>
2022-10-27 22:08:29 -0600

    comenzando quienes-somos.html

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const checkBurger = document.getElementById('check')
+const check = document.getElementById('check')
 const background = document.getElementById('background')
 const burger = document.getElementById('burger')
 const logoClose = document.getElementById('logoClose')
 const navMenu = document.getElementById('nav-menu')
 
-checkBurger.addEventListener('click', function () {
-  if (checkBurger.checked === false) {
+check.addEventListener('click', function () {
+  if (check.checked === false) {
     background.style.backgroundColor = '#4EA539';
     burger.src = 'https://joorkar.github.io/heimdal-green/assets/burger-menu.svg'
     logoClose.src = 'https://joorkar.github.io/heimdal-green/assets/logo-mobile.svg'
