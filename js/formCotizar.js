818ebd4d6762def7de7268a7a0c24644aad6810e
Joorkar Romero <joorkarobrajham@gmail.com>
2022-11-10 23:13:51 -0600

    form add

--- js/formCotizar.js
+++ js/formCotizar.js
@@ -5,10 +5,7 @@ function handleSubmit(e) {
   e.preventDefault()
   const form = new FormData($form)
   console.log(form.get('nombre'));
-  $buttonMailto.setAttribute('href', `mailto:heimdalgreen@gmail.com?subject=Nombre: ${form.get('nombre')} Correo: ${form.get('email')}&body=${form.get('celular')} 
-  ${form.get('servicios')} 
-  ${form.get('ciudad')} 
-  ${form.get('zona')}`)
+  $buttonMailto.setAttribute('href', `mailto:heimdalgreen@gmail.com?subject=Nombre: ${form.get('nombre')} Correo: ${form.get('email')}&body=${form.get('celular')} ${form.get('servicios')} ${form.get('ciudad')} ${form.get('zona')}`)
   $buttonMailto.click()
 
 }
